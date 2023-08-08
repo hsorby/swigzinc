c6d3b38f2c023e57bc772d38b4c5c3a3ba78cb96
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-08-08 13:04:22 +1200

    Make build take into account the release tag.

--- res/setup_linux.sh
+++ res/setup_linux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+release_tag=$1
+
 yum install -y freetype-devel.x86_64 mesa-libGLU-devel.x86_64 libxml2-devel.x86_64
 
 python3.10 -m venv venv
@@ -8,7 +10,7 @@ source venv/bin/activate
 
 pip install ninja
 
-cmake -G Ninja -S res -B build-nonpython
+cmake -G Ninja -S res -B build-nonpython -D RELEASE_TAG=$release_tag
 cmake --build build-nonpython
 
 cd build-nonpython/ZincLibrary/src/build_zinc-build
